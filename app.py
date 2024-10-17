14d51cb3eb0ecea87e6004b530144c0c610b6678
abishake <abishakemuthu@gmail.com>
2024-10-18 00:24:13 +0530

    updated

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
-from fastapi import FastAPI
-import uvicorn
-import sys
 import os
-from fastapi.templating import Jinja2Templates
-from starlette.responses import RedirectResponse
-from fastapi.responses import Response
-from textSummarizer.pipeline.prediction import PredictionPipeline
+import tempfile
+import time
+import streamlit as st
+from parallel_processing import process_document
+from pdf_processor import extract_text_from_pdf
+from preprocess import preprocess_text, extract_keywords
+from model import textsum_model
+from params import summarize
+from database import store_summary_to_db
+from performance import measure_total_performance
 
+st.title('PDF Summarizer📖')
+st.subheader('Looking to save time by summarizing large documents? Simply upload the PDF you wish to summarize and let us handle the rest. ⏳')
 
-text:str = "What is Text Summarization?"
+files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
 
-app = FastAPI()
+if files:
+    model, tokenizer = textsum_model()
 
-@app.get("/", tags=["authentication"])
-async def index():
-    return RedirectResponse(url="/docs")
+    for file in files:
+        file_name = file.name
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file: # Temporary file to store the uploaded PDF.
+            tmp_file.write(file.read())
+            temp_file_path = tmp_file.name  # Temporary file path.
 
+        st.write(f"**File Name:** {file_name}")
+        st.write(f"**File Size:** {round(os.path.getsize(temp_file_path)/1048576, 2)} MB")
 
+        try:
+            def process_document_flow(file_name, temp_file_path):
+                text = extract_text_from_pdf(temp_file_path)
+                processed_text = preprocess_text(text)
+                summary = summarize(processed_text, model, tokenizer)
+                keywords = extract_keywords(processed_text)
+                store_summary_to_db(file_name, temp_file_path, os.path.getsize(temp_file_path), summary, keywords)
+                return summary, keywords
 
-@app.get("/train")
-async def training():
-    try:
-        os.system("python main.py")
-        return Response("Training successful !!")
+            (summary, keywords), total_time = measure_total_performance(process_document_flow, file_name, temp_file_path)
 
-    except Exception as e:
-        return Response(f"Error Occurred! {e}")
-    
+            st.write("### Summary:")
+            st.write(summary)
+            st.write("### Keywords:")
+            st.write(", ".join(keywords))
+            st.write(f"**Total Processing Time:** {round(total_time,2)} seconds")
+            st.write("### Thank You ")
 
+        except Exception as e:
+            st.error(f"Error processing {file_name}: {str(e)}")
 
-
-@app.post("/predict")
-async def predict_route(text):
-    try:
-
-        obj = PredictionPipeline()
-        text = obj.predict(text)
-        return text
-    except Exception as e:
-        raise e
-    
-
-if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+        os.remove(temp_file_path) # Delete the temporary file after processing.
