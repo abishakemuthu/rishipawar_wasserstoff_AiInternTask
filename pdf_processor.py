ef085bf0e9ad11fb00ae760f4f1972e5fa134a2e
abishake <abishakemuthu@gmail.com>
2024-10-17 22:13:52 +0530

    updated

--- pdf_processor.py
+++
@@ -1,6 +0,0 @@
-import pdfplumber
-
-def extract_text_from_pdf(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        text = ''.join([page.extract_text() for page in pdf.pages])
-    return text
